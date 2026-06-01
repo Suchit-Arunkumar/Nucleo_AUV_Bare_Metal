1b5ea5eadf5c5920386ec5b27aefe17b01962502
Suchit Arunkumar <suchit.arunkumar2006@gmail.com>
2026-06-01 13:47:59 +0530

    Day 1: LED blink via raw GPIO and RCC registers

--- firmware/Core/Src/main.c
+++ firmware/Core/Src/main.c
@@ -1,6 +1,23 @@
 #include "stm32f446xx.h"
 
-int main(void) {
-    while(1) {
-    }
+#define LED_PIN (1 << 5)
+
+int main(void){
+
+	RCC->AHB1ENR |= (1 << 0); 		// clk access
+	GPIOA->MODER &= ~(3 << 10);		// op mode gpio
+	GPIOA->MODER |=  (1 << 10);
+
+	while(1){
+
+		GPIOA->BSRR = LED_PIN;
+		for (int i = 0; i < 1000000; i++){
+		}
+
+		GPIOA->BSRR =(1 << 21);
+		for (int i = 0; i < 1000000; i++){
+				}
+
+}
+
 }
