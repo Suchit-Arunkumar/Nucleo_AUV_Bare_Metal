cc65ba109aff4ce2a4f80d0a6d3d14220a271a00
Suchit Arunkumar <suchit.arunkumar2006@gmail.com>
2026-06-01 13:49:29 +0530

    Day 1: LED blink via raw GPIO and RCC registers

--- firmware/Core/Src/main.c
+++ firmware/Core/Src/main.c
@@ -4,17 +4,17 @@
 
 int main(void){
 
-	RCC->AHB1ENR |= (1 << 0); 		// clk access
-	GPIOA->MODER &= ~(3 << 10);		// op mode gpio
-	GPIOA->MODER |=  (1 << 10);
+	RCC->AHB1ENR |= (1 << 0); 		// enable GPIOA clock
+	GPIOA->MODER &= ~(3 << 10);		//clear bits 11:10 for PA5
+	GPIOA->MODER |=  (1 << 10);		// set PA5 as output (01)
 
 	while(1){
 
-		GPIOA->BSRR = LED_PIN;
+		GPIOA->BSRR = LED_PIN;				// set PA5 high
 		for (int i = 0; i < 1000000; i++){
 		}
 
-		GPIOA->BSRR =(1 << 21);
+		GPIOA->BSRR =(1 << 21);				// set PA5 low
 		for (int i = 0; i < 1000000; i++){
 				}
 
